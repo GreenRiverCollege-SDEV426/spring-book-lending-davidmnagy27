8e511810b04830b5a92d4965eefef9e3704cdda3
davidnagy <davidmnagy27@gmail.com>
2020-05-09 13:29:34 -0700

    Add a Service Layer and Controller For Lenders

--- src/main/java/edu/greenriver/it/booklendingspring/controllers/LenderController.java
+++ src/main/java/edu/greenriver/it/booklendingspring/controllers/LenderController.java
@@ -1,4 +1,23 @@
 package edu.greenriver.it.booklendingspring.controllers;
 
-public class LenderController {
+import edu.greenriver.it.booklendingspring.services.LenderService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lenders")
+public class LenderController
+{
+    private LenderService service;
+
+    public LenderController(LenderService service)
+    {
+        this.service=service;
+    }
+    @GetMapping("/all")
+    public String allLenders()
+    {
+        return "lenders/all_lenders";
+    }
 }
--- src/main/java/edu/greenriver/it/booklendingspring/services/LenderService.java
+++ src/main/java/edu/greenriver/it/booklendingspring/services/LenderService.java
@@ -1,4 +1,22 @@
 package edu.greenriver.it.booklendingspring.services;
 
-public class LenderService {
+import edu.greenriver.it.booklendingspring.model.Lender;
+import edu.greenriver.it.booklendingspring.respositories.ILenderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LenderService
+{
+ private ILenderRepository lenderRepository;
+
+ public LenderService(ILenderRepository lenderRepository)
+ {
+     this.lenderRepository = lenderRepository;
+ }
+
+ public Iterable<Lender> getLender()
+ {
+     return lenderRepository.findAll();
+ }
+
 }
